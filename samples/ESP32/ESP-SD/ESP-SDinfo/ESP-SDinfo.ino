fa6a1f8f46f7650124ef787524688b17550a60d3
Tail_BackShape <tail.backshape.bird@gmail.com>
2024-02-09 23:22:30 +0900

    rename sampls > samples
    I mistook the spell...

---
+++ samples/ESP32/ESP-SD/ESP-SDinfo/ESP-SDinfo.ino
@@ -0,0 +1,66 @@
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+void setup() {
+  delay(3000);
+
+  Serial.begin(115200);
+  if (!SD.begin(2)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+  Serial.print("SD Card Type: ");
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+
+  createDir(SD, "/iodhsogibsn");
+  writeFile(SD, "/log.txt", "log start\n");
+}
+
+void loop() {
+}
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
---
+++ samples/RasPiPico/RasPiPico-SPI-SDreadwrite/RasPiPico-SPI-SDreadwrite.ino
@@ -0,0 +1,77 @@
+#include <SPI.h>
+#include <SD.h>
+
+const int _MISO = 4;
+const int _MOSI = 7;
+const int _CS = 5;
+const int _SCK = 6;
+
+File myFile;
+
+SPISettings spisttings(10000000, MSBFIRST, SPI_MODE3);
+
+void setup()
+{
+  Serial.begin(115200);
+
+  while (!Serial)
+  {
+    delay(1);
+  }
+
+  Serial.print("Initializing SD card...");
+  bool sdInitialized = false;
+
+  // setup SPI
+  SPI.setCS(_CS);
+  SPI.setRX(_MISO);
+  SPI.setSCK(_SCK);
+  SPI.setTX(_MOSI);
+  SPI.begin();
+
+  sdInitialized = SD.begin(_CS);
+
+  if (!SD.begin(_CS))
+  {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  if (myFile)
+  {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  }
+  else
+  {
+    Serial.println("error opening test.txt");
+  }
+
+  myFile = SD.open("test.txt");
+  if (myFile)
+  {
+    Serial.println("test.txt:");
+
+    while (myFile.available())
+    {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  }
+  else
+  {
+    Serial.println("error opening test.txt");
+  }
+}
+
+void loop()
+{
+  // write loop!
+}

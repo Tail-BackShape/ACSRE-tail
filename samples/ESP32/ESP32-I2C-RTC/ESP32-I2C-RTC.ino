916d8477e9612ec736edf17e0ffa0c475168bdb5
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-02-11 18:33:57 +0900

    fix ESP32-I2C-RTC.ino
    Guaranteed operation of read_RTC and write_RTC functions

--- samples/ESP32/ESP32-I2C-RTC/ESP32-I2C-RTC.ino
+++ samples/ESP32/ESP32-I2C-RTC/ESP32-I2C-RTC.ino
@@ -2,35 +2,62 @@
 
 #define RTC_addr 0x68
 
-void setup()
-{
+void setup() {
+  Wire.begin();
   Serial.begin(115200);
-  while (!Serial)
-  {
+  while (!Serial) {
     delay(1);
   }
   Serial.println("Start");
 
-  /*write time to DS1307
-  write_RTC(0,0,17, 3, 1,2, 2024);
-  */
+
+  //write time to DS1307
+  //(sec, min, hour, week, day, month, year)
+  write_RTC(0, 35, 18, 3, 11, 2, 24);
 }
 
-void loop()
-{
+void loop() {
 
   // check the function
-  Serial.println("int is 59");
-  Serial.println(int_to_BCD(59));
-  Serial.println(BCD_to_int(int_to_BCD(59)));
-  delay(1000);
+  // Serial.println("int is 59");
+  // Serial.println(int_to_BCD(59));
+  // Serial.println(BCD_to_int(int_to_BCD(59)));
 
   // read rtc time
-  // int sec = read_RTC(0x00);
+  // format is BCD
+  byte sec_r = read_RTC(0x00);
+  byte min_r = read_RTC(0x01);
+  byte hour_r = read_RTC(0x02);
+  // byte day_r = read_RTC(0x03);
+  byte date_r = read_RTC(0x04);
+  byte month_r = read_RTC(0x05);
+  byte year_r = read_RTC(0x06);
+
+  // convert BCD to int
+  int sec = BCD_to_int(sec_r);
+  int min = BCD_to_int(min_r);
+  int hour = BCD_to_int(hour_r);
+  // int day = BCD_to_int(day_r); // san, mon...
+  int date = BCD_to_int(date_r);
+  int month = BCD_to_int(month_r);
+  int year = BCD_to_int(year_r);
+
+  Serial.print(year);
+  Serial.print("/");
+  Serial.print(month);
+  Serial.print("/");
+  Serial.print(date);
+  Serial.print("/");
+  Serial.print(hour);
+  Serial.print(":");
+  Serial.print(min);
+  Serial.print(":");
+  Serial.println(sec);
+
+  delay(1000);
 }
 
-byte read_RTC(byte addr)
-{
+byte read_RTC(byte addr) {
   Wire.beginTransmission(RTC_addr);
   Wire.write(addr);
   Wire.endTransmission(false);
@@ -39,21 +66,20 @@ byte read_RTC(byte addr)
   return data;
 }
 
-void write_RTC(int sec, int min, int hour, int week, int day, int month, int year)
-{
+void write_RTC(int sec, int min, int hour, int week, int day, int month, int year) {
   // DS1307 memory is BCD
   // int to BCD
   byte sec_w = int_to_BCD(sec);
   byte min_w = int_to_BCD(min);
   byte hour_w = int_to_BCD(hour);
-  byte week_w = int_to_BCD(week); // week is date
+  byte week_w = int_to_BCD(week);  // week is date
   byte day_w = int_to_BCD(day);
   byte month_w = int_to_BCD(month);
   byte year_w = int_to_BCD(year);
 
   // update RTC time
   Wire.beginTransmission(RTC_addr);
-  Wire.write(0x00); // register header addr
+  Wire.write(0x00);  // register header addr
   Wire.write(sec_w);
   Wire.write(min_w);
   Wire.write(hour_w);
@@ -64,21 +90,19 @@ void write_RTC(int sec, int min, int hour, int week, int day, int month, int yea
   Wire.endTransmission();
 }
 
-int int_to_BCD(byte data)
-{
+int int_to_BCD(byte data) {
   // e.g. data = 59
-  uint8_t ones = data % 10;          // 9
-  uint8_t tens = (data - ones) / 10; // 5
-  uint8_t BCD = (tens << 4) | ones;  // 89 = 0x59
+  uint8_t ones = data % 10;           // 9
+  uint8_t tens = (data - ones) / 10;  // 5
+  uint8_t BCD = (tens << 4) | ones;   // 89 = 0x59
 
   return BCD;
 }
 
-int BCD_to_int(byte BCD)
-{
+int BCD_to_int(byte BCD) {
   // e.g. BCD =  0x89(59, B01011001)
-  uint8_t ones = BCD & B1111;     // 9
-  uint8_t tens = (BCD >> 4) * 10; // 50
+  uint8_t ones = BCD & B1111;      // 9
+  uint8_t tens = (BCD >> 4) * 10;  // 50
   uint8_t data = tens + ones;
 
   return data;

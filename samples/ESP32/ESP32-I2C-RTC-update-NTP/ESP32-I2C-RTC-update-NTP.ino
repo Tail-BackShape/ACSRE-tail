3eb788ae6c57da7b90b52f5d8766fa067147119b
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-02-12 23:10:45 +0900

    testtest

--- samples/ESP32/ESP32-I2C-RTC-update-NTP/ESP32-I2C-RTC-update-NTP.ino
+++ samples/ESP32/ESP32-I2C-RTC-update-NTP/ESP32-I2C-RTC-update-NTP.ino
@@ -1,121 +1,115 @@
 #include <Wire.h>
-#define RTC_address 0x68
 
-uint8_t REG_table[7];
+#define RTC_addr 0x68
 
-void setup() {
+void setup()
+{
   Wire.begin();
   Serial.begin(115200);
+  while (!Serial)
+  {
+    delay(1);
+  }
+  Serial.println("Start");
 
-  Serial.println("start");
-
-
-  Wire.beginTransmission(RTC_address);
-  Wire.write(0x00);  //Register 先頭アドレス
-  Wire.write(0x00);  //second
-  Wire.write(0x36);  //minute
-  Wire.write(0x20);  //hour
-  Wire.write(0x04);  //week
-  Wire.write(0x24);  //day
-  Wire.write(0x12);  //month
-  Wire.write(0x22);  //year
-  Wire.endTransmission();
+  // write time to DS1307
+  //(sec, min, hour, week, day, month, year)
+  write_RTC(0, 35, 18, 3, 11, 2, 24);
 }
 
-void loop() {
-  //一つずつ読む場合
-  byte sec_d = read_rtc(0x00);
-  byte min_d = read_rtc(0x01);
-  byte hour_d = read_rtc(0x02);
-  byte day_d = read_rtc(0x04);
-  byte month_d = read_rtc(0x05);
+void loop()
+{
 
+  // check the function
+  // Serial.println("int is 59");
+  // Serial.println(int_to_BCD(59));
+  // Serial.println(BCD_to_int(int_to_BCD(59)));
 
-  int sec = dec_to_int(sec_d);
-  int min = dec_to_int(min_d);
-  int hour = dec_to_int(hour_d);
-  int day = dec_to_int(day_d);
-  int month = dec_to_int(month_d);
+  // read rtc time
+  // format is BCD
+  byte sec_r = read_RTC(0x00);
+  byte min_r = read_RTC(0x01);
+  byte hour_r = read_RTC(0x02);
+  // byte day_r = read_RTC(0x03);
+  byte date_r = read_RTC(0x04);
+  byte month_r = read_RTC(0x05);
+  byte year_r = read_RTC(0x06);
 
-  Serial.print(sec);
-  Serial.print("秒, ");
-  Serial.print(min);
-  Serial.print("分, ");
-  Serial.print(hour);
-  Serial.print("時, ");
-  Serial.print(day);
-  Serial.print("日, ");
+  // convert BCD to int
+  int sec = BCD_to_int(sec_r);
+  int min = BCD_to_int(min_r);
+  int hour = BCD_to_int(hour_r);
+  // int day = BCD_to_int(day_r); // san, mon...
+  int date = BCD_to_int(date_r);
+  int month = BCD_to_int(month_r);
+  int year = BCD_to_int(year_r);
+
+  Serial.print(year);
+  Serial.print("/");
   Serial.print(month);
-  Serial.print("月");
-
-
-
-
-  //一括で読む場合
-  /*
-    byte k[20];
-    read_rtc_multibyte(k, 0x00, 7);
-    for (int i = 0; i < 7; i++) {
-    Serial.print(k[i]);
-    Serial.print(", ");
-    }
-
-    int sec = dec_to_int(k[0]);
-    int min = dec_to_int(k[1]);
-    int hour = dec_to_int(k[2]);
-    int day = dec_to_int(k[4]);
-    int month = dec_to_int(k[5]);
-
-    Serial.print(sec);
-    Serial.print("秒, ");
-    Serial.print(min);
-    Serial.print("分, ");
-    Serial.print(hour);
-    Serial.print("時, ");
-    Serial.print(day);
-    Serial.print("日, ");
-    Serial.print(month);
-    Serial.print("月");
-  */
-
-
-  Serial.println("");
+  Serial.print("/");
+  Serial.print(date);
+  Serial.print("/");
+  Serial.print(hour);
+  Serial.print(":");
+  Serial.print(min);
+  Serial.print(":");
+  Serial.println(sec);
 
   delay(1000);
 }
 
-int dec_to_int(byte dec) {
-  int data;
-  data = (dec >> 4) * 10 + (dec & 0x0F);
+byte read_RTC(byte addr)
+{
+  Wire.beginTransmission(RTC_addr);
+  Wire.write(addr);
+  Wire.endTransmission(false);
+  Wire.requestFrom(RTC_addr, 1, true);
+  byte data = Wire.read();
   return data;
 }
-void write_rtc(byte add, byte data) {
-  Wire.beginTransmission(RTC_address);
-  Wire.write(add);
-  Wire.write(data);
+
+void write_RTC(int sec, int min, int hour, int week, int day, int month, int year)
+{
+  // DS1307 memory is BCD
+  // int to BCD
+  byte sec_w = int_to_BCD(sec);
+  byte min_w = int_to_BCD(min);
+  byte hour_w = int_to_BCD(hour);
+  byte week_w = int_to_BCD(week); // week is date
+  byte day_w = int_to_BCD(day);
+  byte month_w = int_to_BCD(month);
+  byte year_w = int_to_BCD(year);
+
+  // update RTC time
+  Wire.beginTransmission(RTC_addr);
+  Wire.write(0x00); // register header addr
+  Wire.write(sec_w);
+  Wire.write(min_w);
+  Wire.write(hour_w);
+  Wire.write(week_w);
+  Wire.write(day_w);
+  Wire.write(month_w);
+  Wire.write(year_w);
   Wire.endTransmission();
 }
-byte read_rtc(byte add) {
-  byte k = 0;
-  Wire.beginTransmission(RTC_address);
-  Wire.write(add);
-  Wire.endTransmission();
-  Wire.requestFrom(RTC_address, 1);
-  while (Wire.available()) {
-    k = Wire.read();
-  }
-  return k;
+
+int int_to_BCD(byte data)
+{
+  // e.g. data = 59
+  uint8_t ones = data % 10;          // 9
+  uint8_t tens = (data - ones) / 10; // 5
+  uint8_t BCD = (tens << 4) | ones;  // 89 = 0x59
+
+  return BCD;
 }
 
+int BCD_to_int(byte BCD)
+{
+  // e.g. BCD =  0x89(59, B01011001)
+  uint8_t ones = BCD & B1111;     // 9
+  uint8_t tens = (BCD >> 4) * 10; // 50
+  uint8_t data = tens + ones;
 
-void read_rtc_multibyte(byte *array1, byte add, int num) {
-  Wire.beginTransmission(RTC_address);
-  Wire.write(add);
-  Wire.endTransmission();
-  Wire.requestFrom(RTC_address, num);
-  int i = 0;
-  while (Wire.available()) {
-    array1[i] = Wire.read();
-    i++;
-  }
+  return data;
 }
